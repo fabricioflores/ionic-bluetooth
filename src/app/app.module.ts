181397cb5ed92b66e84229ad821f3dfd4f7391b8
Fabricio Flores <fabrifloresg@gmail.com>
2020-05-08 12:00:51 -0500

    Use bluetooth serial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    BluetoothSerial,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
+import { ToastController } from '@ionic/angular';
+
+interface Device {
+  "class": number,
+  "id": string,
+  "address": string,
+  "name": string
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
 
-  constructor() {}
+  pairedList: Device[];
+  listToggle: boolean = false;
+  pairedDeviceID: number = null;
+  connectedDevice: boolean = false;
+  response = {};
+
+  constructor(private bluetoothSerial: BluetoothSerial, private toastController: ToastController, private zone: NgZone) {
+    this.checkBluetoothEnabled();
+  }
+
+  checkBluetoothEnabled() {
+    this.bluetoothSerial.isEnabled().then(() => {
+      this.listPairedDevices();
+    }, () => {
+      this.onError('Por favor habilite el Bluetooth');
+    });
+  }
+
+  listPairedDevices() {
+    this.bluetoothSerial.list().then(success => {
+      this.pairedList = success;
+      this.listToggle = true;
+    }, () => {
+      this.onError('Por favor habilite el Bluetooth');
+      this.listToggle = false;
+    });
+  }
+
+  selectDevice() {
+    let connectedDevice = this.pairedList[this.pairedDeviceID];
+    if (!connectedDevice || !connectedDevice.address) {
+      this.onError('Seleccione un dispositivo pareado para conectar');
+      return;
+    }
+    let address = connectedDevice.address;
+    this.connect(address);
+  }
+
+  connect(address) {
+    this.bluetoothSerial.connect(address).subscribe(() => {
+      this.deviceConnected();
+      this.response = {};
+      this.connectedDevice = true;
+      this.onSuccess('Dispositivo conectado');
+    }, () => {
+      this.onError('Seleccione un dispositivo pareado para conectar');
+    });
+  }
+
+  deviceConnected() {
+    this.bluetoothSerial.subscribe('\r\n').subscribe(success => {
+      this.zone.run(() => {
+        this.response[success] = this.response[success] === undefined ? 0 : this.response[success] + 1;
+      });
+    }, (error) => {
+      this.onError(error);
+    });
+  }
+
+  disconnect() {
+    this.bluetoothSerial.disconnect();
+    this.connectedDevice = false;
+    this.onSuccess('Dispositivo desconectado');
+  }
+
+  onSuccess(message: string) {
+    this.toastController.create({
+      message,
+      duration: 2000,
+      color: 'success'
+    }).then(toast => {
+      toast.present();
+    })
+  }
+
+  onError(message: string) {
+    this.toastController.create({
+      message,
+      duration: 2000,
+      color: 'danger'
+    }).then(toast => {
+      toast.present();
+    })
+  }
 
 }
